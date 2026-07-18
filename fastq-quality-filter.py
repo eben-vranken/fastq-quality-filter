a48bb0badd6f87ff157f98516d56a7419229a122
eben-vranken <vranken.eben@outlook.com>
2026-07-18 21:24:05 +0200

    feat: initial cli entrypoint with arguments

--- fastq-quality-filter.py
+++ fastq-quality-filter.py
@@ -1,2 +1,12 @@
+from argparse import ArgumentParser
+
+def parse_args():
+    parser = ArgumentParser()
+
+    parser.add_argument("path", help="Path to the FastQ file to be parsed.")
+    parser.add_argument("-q", "--quality", required=True, help="Minimum quality to be shown")
+
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    pass
+    args = parse_args()
