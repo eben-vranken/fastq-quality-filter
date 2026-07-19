0c5a21cb6412dd607ae9472e2c9e6237a8d7612e
eben-vranken <vranken.eben@outlook.com>
2026-07-19 14:50:23 +0200

    feat: add simple (and not future proof) fastq data parser

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,24 @@
 def parse_fastq(file_path):
+    reads = []
+    read = {}
+
+    line_count = 0
     with open(file_path) as fp:
         for line in fp:
-            print(line.strip())
+            line = line.strip()
+            if line_count == 0:
+                read["title"] = line
+            elif line_count == 1:
+                read["sequence"] = line
+            elif line_count == 3:
+                read["phred_score"] = line
+
+            line_count += 1
+            
+            if line_count == 4:
+                line_count = 0
+                reads.append(read)
+                read = {}
+
+    for read in reads:
+        print(read)
