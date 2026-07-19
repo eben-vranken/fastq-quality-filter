c6568f41986b0505a7668425179e31edbc24a6eb
eben-vranken <vranken.eben@outlook.com>
2026-07-19 15:10:07 +0200

    feat: basic phred q filtering

--- fastq-quality-filter.py
+++ fastq-quality-filter.py
@@ -13,4 +13,6 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
 
-    parser.parse_fastq(args.path)
+    reads = parser.parse_fastq(args.path)
+
+    filtered_reads = parser.filter_reads(reads, int(args.quality))
--- src/parser.py
+++ src/parser.py
@@ -20,5 +20,25 @@ def parse_fastq(file_path):
                 reads.append(read)
                 read = {}
 
+    return reads
+
+def filter_reads(reads, quality):
+    filtered_reads = []
     for read in reads:
+        filtered_read = {}
+        filtered_read["title"] = read["title"]
+        filtered_read["sequence"] = ""
+        filtered_read["phred_score"] = ""
+        print(len(read["sequence"]))
+        print(len(read["phred_score"]))
+        for i in range(0, len(read["sequence"])):
+            phred = ord(read["phred_score"][i]) - 33
+            if phred >= quality:
+                filtered_read["sequence"] += read["sequence"][i]
+                filtered_read["phred_score"] += read["phred_score"][i]
+
+        filtered_reads.append(filtered_read)
+
+    for read in filtered_reads:
         print(read)
+        print()
